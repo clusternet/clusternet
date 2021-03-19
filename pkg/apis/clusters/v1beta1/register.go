38562602e1f42df43d909de0740d89b3ca9084c9
Di Xu <stephenhsu90@gmail.com>
2021-03-19 20:33:37 +0800

    add api ManagedCluster

--- pkg/apis/clusters/v1beta1/register.go
+++ pkg/apis/clusters/v1beta1/register.go
@@ -53,6 +53,8 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 	scheme.AddKnownTypes(SchemeGroupVersion,
 		&ClusterRegistrationRequest{},
 		&ClusterRegistrationRequestList{},
+		&ManagedCluster{},
+		&ManagedClusterList{},
 	)
 	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
 	return nil
--- pkg/apis/clusters/v1beta1/types.go
+++ pkg/apis/clusters/v1beta1/types.go
@@ -59,3 +59,39 @@ type ClusterRegistrationRequestList struct {
 	metav1.ListMeta `json:"metadata,omitempty"`
 	Items           []ClusterRegistrationRequest `json:"items"`
 }
+
+// ManagedClusterSpec defines the desired state of ManagedCluster
+type ManagedClusterSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+// ManagedClusterStatus defines the observed state of ManagedCluster
+type ManagedClusterStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:scope="Namespaced"
+
+// ManagedCluster is the Schema for the managedclusters API
+type ManagedCluster struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ManagedClusterSpec   `json:"spec,omitempty"`
+	Status ManagedClusterStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// ManagedClusterList contains a list of ManagedCluster
+type ManagedClusterList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ManagedCluster `json:"items"`
+}
