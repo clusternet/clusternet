7dc9675c6ac2083bacd90e6e89f8f0151d13603e
Di Xu <stephenhsu90@gmail.com>
2021-07-15 22:04:29 +0800

    add tests for getExtraFromHeaders

--- pkg/exchanger/exchanger_test.go
+++ pkg/exchanger/exchanger_test.go
@@ -19,6 +19,7 @@ package exchanger
 import (
 	"net/http"
 	"net/url"
+	"reflect"
 	"testing"
 )
 
@@ -84,6 +85,97 @@ func TestAuthorizer(t *testing.T) {
 			}
 			if got1 != tt.authed {
 				t.Errorf("authorizer() got1 = %v, clusterID %v", got1, tt.authed)
+			}
+		})
+	}
+}
+
+// modified on k8s.io/apiserver/pkg/authentication/request/headerrequest/requestheader_test.go
+func TestGetExtraFromHeaders(t *testing.T) {
+	tests := []struct {
+		name           string
+		header         http.Header
+		headerPrefixes []string
+		wantedExtra    map[string][]string
+	}{
+		{
+			name:        "empty",
+			wantedExtra: map[string][]string{},
+		},
+		{
+			name:           "extra not match 1",
+			header:         http.Header{"X-Remote-User": {"Bob"}},
+			headerPrefixes: []string{"X-Remote-Extra-"},
+			wantedExtra:    map[string][]string{},
+		},
+		{
+			name: "exact not match 2",
+			header: http.Header{
+				"X-Remote-Extra":          {"", "First header, second value"},
+				"A-Second-X-Remote-Extra": {"Second header, first value", "Second header, second value"},
+				"Another-X-Remote-Extra":  {"Third header, first value"}},
+			headerPrefixes: []string{"X-Remote-Extra-"},
+			wantedExtra:    map[string][]string{},
+		},
+		{
+			name: "extra prefix matches",
+			header: http.Header{
+				"X-Remote-Extra-User": {"Bob", "Tom"},
+			},
+			headerPrefixes: []string{"X-Remote-Extra-"},
+			wantedExtra: map[string][]string{
+				"user": {"Bob", "Tom"},
+			},
+		},
+		{
+			name: "extra prefix matches case-insensitive",
+			header: http.Header{
+				"X-Remote-User":         {"Bob"},
+				"X-Remote-Group-1":      {"one-a", "one-b"},
+				"X-Remote-Group-2":      {"two-a", "two-b"},
+				"X-Remote-extra-1-key1": {"alfa", "bravo"},
+				"X-Remote-Extra-1-Key2": {"charlie", "delta"},
+				"X-Remote-Extra-1-":     {"india", "juliet"},
+				"X-Remote-extra-2-":     {"kilo", "lima"},
+				"X-Remote-extra-2-Key1": {"echo", "foxtrot"},
+				"X-Remote-Extra-2-key2": {"golf", "hotel"},
+			},
+			headerPrefixes: []string{"X-Remote-Extra-1-", "X-Remote-Extra-2-"},
+			wantedExtra: map[string][]string{
+				"key1": {"alfa", "bravo", "echo", "foxtrot"},
+				"key2": {"charlie", "delta", "golf", "hotel"},
+				"":     {"india", "juliet", "kilo", "lima"},
+			},
+		},
+		{
+			name: "escaped extra keys",
+			header: http.Header{
+				"X-Remote-User":                                            {"Bob"},
+				"X-Remote-Group":                                           {"one-a", "one-b"},
+				"X-Remote-Extra-Alpha":                                     {"alphabetical"},
+				"X-Remote-Extra-Alph4num3r1c":                              {"alphanumeric"},
+				"X-Remote-Extra-Percent%20encoded":                         {"percent encoded"},
+				"X-Remote-Extra-Almost%zzpercent%xxencoded":                {"not quite percent encoded"},
+				"X-Remote-Extra-Example.com%2fpercent%2520encoded":         {"url with double percent encoding"},
+				"X-Remote-Extra-Example.com%2F%E4%BB%8A%E6%97%A5%E3%81%AF": {"url with unicode"},
+				"X-Remote-Extra-Abc123!#$+.-_*\\^`~|'":                     {"header key legal characters"},
+			},
+			headerPrefixes: []string{"X-Remote-Extra-"},
+			wantedExtra: map[string][]string{
+				"alpha":                         {"alphabetical"},
+				"alph4num3r1c":                  {"alphanumeric"},
+				"percent encoded":               {"percent encoded"},
+				"almost%zzpercent%xxencoded":    {"not quite percent encoded"},
+				"example.com/percent%20encoded": {"url with double percent encoding"},
+				"example.com/今日は":               {"url with unicode"},
+				"abc123!#$+.-_*\\^`~|'":         {"header key legal characters"},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getExtraFromHeaders(tt.header, tt.headerPrefixes); !reflect.DeepEqual(got, tt.wantedExtra) {
+				t.Errorf("getExtraFromHeaders() = %v, wantedExtra %v", got, tt.wantedExtra)
 			}
 		})
 	}
