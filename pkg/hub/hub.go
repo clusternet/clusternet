2b00cb21c45948d5db0029670efccfdbca27c4df
qmhu <brillantroad@gmail.com>
2021-09-01 11:45:14 +0800

    add cluster lifecycle controller and cluster.status.conditions

--- pkg/hub/hub.go
+++ pkg/hub/hub.go
@@ -20,14 +20,22 @@ import (
 	"context"
 	"time"
 
+	corev1 "k8s.io/api/core/v1"
 	crdclientset "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
 	crdinformers "k8s.io/apiextensions-apiserver/pkg/client/informers/externalversions"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	genericapiserver "k8s.io/apiserver/pkg/server"
 	utilfeature "k8s.io/apiserver/pkg/util/feature"
 	kubeInformers "k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/scheme"
+	v1core "k8s.io/client-go/kubernetes/typed/core/v1"
+	"k8s.io/client-go/tools/record"
 	"k8s.io/klog/v2"
 
+	appsapi "github.com/clusternet/clusternet/pkg/apis/apps/v1alpha1"
+	clusterapi "github.com/clusternet/clusternet/pkg/apis/clusters/v1beta1"
+	"github.com/clusternet/clusternet/pkg/controllers/clusters/clusterlifecycle"
 	"github.com/clusternet/clusternet/pkg/features"
 	clusternet "github.com/clusternet/clusternet/pkg/generated/clientset/versioned"
 	informers "github.com/clusternet/clusternet/pkg/generated/informers/externalversions"
@@ -60,6 +68,10 @@ type Hub struct {
 	crrApprover *approver.CRRApprover
 	deployer    *deployer.Deployer
 
+	clusterLifecycle *clusterlifecycle.Controller
+
+	recorder record.EventRecorder
+
 	socketConnection bool
 	deployerEnabled  bool
 }
@@ -78,6 +90,20 @@ func NewHub(ctx context.Context, opts *options.HubServerOptions) (*Hub, error) {
 	kubeclient := kubernetes.NewForConfigOrDie(config)
 	clusternetclient := clusternet.NewForConfigOrDie(config)
 	crdclient := crdclientset.NewForConfigOrDie(config)
+
+	//deployer.broadcaster.StartStructuredLogging(5)
+	broadcaster := record.NewBroadcaster()
+	if kubeclient != nil {
+		klog.Infof("sending events to api server")
+		broadcaster.StartRecordingToSink(&v1core.EventSinkImpl{
+			Interface: kubeclient.CoreV1().Events(""),
+		})
+	} else {
+		klog.Warningf("no api server defined - no events will be sent to API server.")
+	}
+	utilruntime.Must(appsapi.AddToScheme(scheme.Scheme))
+	utilruntime.Must(clusterapi.AddToScheme(scheme.Scheme))
+	recorder := broadcaster.NewRecorder(scheme.Scheme, corev1.EventSource{Component: "clusternet-hub"})
 
 	// creates the informer factory
 	kubeInformerFactory := kubeInformers.NewSharedInformerFactory(kubeclient, known.DefaultResync)
@@ -110,7 +136,7 @@ func NewHub(ctx context.Context, opts *options.HubServerOptions) (*Hub, error) {
 		clusternetInformerFactory.Apps().V1alpha1().Localizations().Informer()
 		clusternetInformerFactory.Apps().V1alpha1().Globalizations().Informer()
 
-		d, err = deployer.NewDeployer(ctx, kubeclient, clusternetclient, clusternetInformerFactory, kubeInformerFactory)
+		d, err = deployer.NewDeployer(ctx, kubeclient, clusternetclient, clusternetInformerFactory, kubeInformerFactory, recorder)
 		if err != nil {
 			return nil, err
 		}
@@ -119,6 +145,8 @@ func NewHub(ctx context.Context, opts *options.HubServerOptions) (*Hub, error) {
 	if utilfeature.DefaultFeatureGate.Enabled(features.ShadowAPI) {
 		clusternetInformerFactory.Apps().V1alpha1().Manifests().Informer()
 	}
+
+	clusterLifecycle := clusterlifecycle.NewController(ctx, clusternetclient, clusternetInformerFactory.Clusters().V1beta1().ManagedClusters(), recorder)
 
 	hub := &Hub{
 		ctx:                       ctx,
@@ -132,7 +160,9 @@ func NewHub(ctx context.Context, opts *options.HubServerOptions) (*Hub, error) {
 		crdInformerFactory:        crdInformerFactory,
 		socketConnection:          socketConnection,
 		deployer:                  d,
+		recorder:                  recorder,
 		deployerEnabled:           deployerEnabled,
+		clusterLifecycle:          clusterLifecycle,
 	}
 
 	// Start the informer factories to begin populating the informer caches
@@ -154,6 +184,8 @@ func (hub *Hub) Run() error {
 			hub.deployer.Run(DefaultThreadiness)
 		}()
 	}
+
+	go hub.clusterLifecycle.Run(DefaultThreadiness, hub.ctx.Done())
 
 	return hub.RunAPIServer()
 }
