6a77a550944004b7c4c1951fae66458ea740acce
Di Xu <stephenhsu90@gmail.com>
2021-10-23 15:21:24 +0800

    removing api import path restriction

--- pkg/apis/proxies/v1alpha1/doc.go
+++ pkg/apis/proxies/v1alpha1/doc.go
@@ -21,4 +21,4 @@ limitations under the License.
 // +groupName=proxies.clusternet.io
 
 // Package v1alpha1 is the v1alpha1 version of the API.
-package v1alpha1 // import "github.com/clusternet/clusternet/pkg/apis/proxies/v1alpha1"
+package v1alpha1
