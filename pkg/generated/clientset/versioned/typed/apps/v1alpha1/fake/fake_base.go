104d838148fbb33a827f36203976812e22f7717e
aven-ai <as1919@qq.com>
2022-05-29 21:20:02 +0800

    ADD Feature: Aggregate multi clusters resource status, Update apis status definition.
    Signed-off-by: aven-ai <as1919@qq.com>

--- pkg/generated/clientset/versioned/typed/apps/v1alpha1/fake/fake_base.go
+++ pkg/generated/clientset/versioned/typed/apps/v1alpha1/fake/fake_base.go
@@ -101,6 +101,18 @@ func (c *FakeBases) Update(ctx context.Context, base *v1alpha1.Base, opts v1.Upd
 	return obj.(*v1alpha1.Base), err
 }
 
+// UpdateStatus was generated because the type contains a Status member.
+// Add a +genclient:noStatus comment above the type to avoid generating UpdateStatus().
+func (c *FakeBases) UpdateStatus(ctx context.Context, base *v1alpha1.Base, opts v1.UpdateOptions) (*v1alpha1.Base, error) {
+	obj, err := c.Fake.
+		Invokes(testing.NewUpdateSubresourceAction(basesResource, "status", c.ns, base), &v1alpha1.Base{})
+
+	if obj == nil {
+		return nil, err
+	}
+	return obj.(*v1alpha1.Base), err
+}
+
 // Delete takes name of the base and deletes it. Returns an error if one occurs.
 func (c *FakeBases) Delete(ctx context.Context, name string, opts v1.DeleteOptions) error {
 	_, err := c.Fake.
